9282d036a4afed64d113fd6c045bf86340f51c54
Tilak-csd <tilakgubhaju3@gmail.com>
2025-12-10 07:49:10 +0545

    added

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,13 +2,12 @@ import React from 'react'
 
 export default function Hero() {
   return (
-    <div className='bg-gray-200 h-[calc(100vh-60px)] w-full grid grid-cols-6 grid-rows-4'>
+    <div className='bg-gray-200 h-[calc(100vh-60px)] w-full grid grid-cols-4 sm:grid-cols-6 grid-rows-4'>
 
       <FirstComponent />
       <SecondComponent />
       <ThirdComponent />
-      <div className="col-span-3 row-span-4 col-start-4 row-start-1 bg-blue-300">5</div>
-
+      <FourthComponent />
     </div>
   )
 }
@@ -32,20 +31,20 @@ function FirstComponent() {
 }
 
 function SecondComponent() {
-  return <div className="col-span-2 row-span-3 flex justify-around flex-col items-start ">
-    <div className='w-full justify-start gap-10 flex'>
+  return <div className="sm:col-span-2 col-span-3 row-span-3 flex justify-around flex-col items-start ">
+    <div className='w-full justify-start gap-4 sm:gap-10 flex'>
       <div className='flex flex-col items-start'>
-        <div className='text-3xl font-light text-gray-600 flex justify-start'>+<span className='text-6xl'>10</span></div>
-        <div className='text-gray-400 text-md ml-8'>Projects Completed</div>
+        <div className='text-2xl sm:text-3xl font-light text-gray-600 flex justify-start'>+<span className='text-6xl'>10</span></div>
+        <div className='text-gray-400 text-sm sm:text-md ml-4 sm:ml-8'>Projects Completed</div>
       </div>
       <div>
         <div className='text-3xl font-light text-gray-600 flex justify-start'>+<span className='text-6xl'>5</span></div>
-        <div className='text-gray-400 text-md ml-5'>Collaboration</div>
+        <div className='text-gray-400 text-sm sm:text-md ml-5'>Collaboration</div>
       </div>
     </div>
     <div className='flex flex-col justify-center items-start'>
-      <h1 className='text-9xl font-light'>Hello</h1>
-      <div className='flex justify-center items-center'>
+      <h1 className='text-8xl sm:text-9xl font-light'>Hello</h1>
+      <div className='flex justify-center items-start md:items-start'>
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-6">
           <path fill-rule="evenodd" d="M4.25 12a.75.75 0 0 1 .75-.75h14a.75.75 0 0 1 0 1.5H5a.75.75 0 0 1-.75-.75Z" clip-rule="evenodd" />
         </svg>
@@ -65,5 +64,10 @@ function ThirdComponent() {
       </svg>
     </div>
   </div>
+
+}
+
+function FourthComponent() {
+  return <div className="sm:block hidden col-span-3 row-span-4 col-start-4 row-start-1 bg-blue-300">5</div>
 
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -18,14 +18,14 @@ export default function Navbar() {
   return (
     <div className="relative">
       {/* Navbar itself */}
-      <div className="relative flex justify-between items-center h-[60px] md:h-12 w-full md:px-6 px-4 border-b md:border-0 border-gray-200 z-20 bg-white">
+      <div className="relative flex justify-between items-center h-[60px] w-full md:px-6 px-4 border-b md:border-0 border-gray-200 z-20 bg-white">
         <p className="font-rosca text-xl  md:text-2xl">Tilak Gubhaju</p>
         {open ? (<CloseMenu setOpen={setOpen} />) : (<OpenMenu setOpen={setOpen} />)}
         {/* Desktop Menu Navbar */}
         <div className='hidden relative md:flex justify-center items-center gap-3 h-full bg-gray-200 px-5 rounded-t-xl'>
           <NavLink />
         </div>
-        <a href="" className='relative flex justify-center items-center '>
+        <a href="" className='hidden md:flex relative justify-center items-center '>
           Book a call
           <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-5 w-4 h-4">
             <path fill-rule="evenodd" d="M8.25 3.75H19.5a.75.75 0 0 1 .75.75v11.25a.75.75 0 0 1-1.5 0V6.31L5.03 20.03a.75.75 0 0 1-1.06-1.06L17.69 5.25H8.25a.75.75 0 0 1 0-1.5Z" clip-rule="evenodd" />
@@ -39,7 +39,13 @@ export default function Navbar() {
         className={`absolute md:hidden flex justify-center items-center flex-col gap-5 top-[60px] left-0 w-full h-[calc(100vh-60px)] bg-white z-10 transition-all duration-500 ease-in-out
           ${open ? 'opacity-100 translate-y-0' : 'opacity-0 -translate-y-full pointer-events-none'} `} >
         <NavLink />
-        <Buttons label={'Contact'} />
+        <a href="" className='flex relative justify-center items-center '>
+          Book a call
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-5 w-4 h-4">
+            <path fill-rule="evenodd" d="M8.25 3.75H19.5a.75.75 0 0 1 .75.75v11.25a.75.75 0 0 1-1.5 0V6.31L5.03 20.03a.75.75 0 0 1-1.06-1.06L17.69 5.25H8.25a.75.75 0 0 1 0-1.5Z" clip-rule="evenodd" />
+          </svg>
+          <span class="absolute bottom-0 left-0 h-0.5 w-[80%] bg-black"></span>
+        </a>
       </div>
     </div>
   );
@@ -85,7 +91,7 @@ function OpenMenu({ setOpen }) {
 
 function CloseMenu({ setOpen }) {
   return (
-    <div className="p-2 mr-3 cursor-pointer md:hidden" onClick={() => setOpen(prev => !prev)}>
+    <div className="p-2 cursor-pointer md:hidden" onClick={() => setOpen(prev => !prev)}>
       <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
         <path strokeLinecap="round" strokeLinejoin="round" d="M6 18 18 6M6 6l12 12" />
       </svg>
